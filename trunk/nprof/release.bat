e5d7aa836ed3d9f04075bb30fabe9b7d2c3a001f
mmastrac <mmastrac@0dbee6a0-3221-0410-985d-3ba8a6ab8d54>
2003-12-26 22:04:40 +0000

    Upgrading build system

---
+++ trunk/nprof/release.bat
@@ -0,0 +1,7 @@
+set VERSION=0.5-alpha
+
+if "%1"=="upload" (
+	build\NAnt\NAnt -f:build\nprof.build -D:build.version=%VERSION% -D:build.configuration=release upload
+) else (
+	build\NAnt\NAnt -f:build\nprof.build -D:build.version=%VERSION% -D:build.configuration=release package
+)
