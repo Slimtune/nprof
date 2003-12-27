f1d4892d72dedc4ed4b0930f14d138eeabe5d900
mmastrac <mmastrac@0dbee6a0-3221-0410-985d-3ba8a6ab8d54>
2003-12-27 21:15:55 +0000

    Some new tests to run

--- trunk/nprof/TestProfilee/TestClass.cs
+++ trunk/nprof/TestProfilee/TestClass.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Threading;
 
 namespace TestProfilee
 {
@@ -8,62 +7,30 @@ namespace TestProfilee
 	/// </summary>
 	public class TestClass
 	{
-		public ManualResetEvent _mreA = new ManualResetEvent( false );
-		public ManualResetEvent _mreB = new ManualResetEvent( false );
-
-		public TestClass()
-		{
-		}
-
-		public int Run()
-		{
-			Thread tA = new Thread( new ThreadStart( ThreadA ) );
-			Thread tB = new Thread( new ThreadStart( ThreadB ) );
-
-			Thread.CurrentThread.Name = "Main thread";
-			tA.Name = "Thread A";
-			tB.Name = "Thread B";
-
-			tA.Start();
-			tB.Start();
-			System.Console.WriteLine( "Suspending thread A" );
-			Thread.Sleep( 100 );
-			tA.Suspend();
-			Thread.Sleep( 2500 );
-			System.Console.WriteLine( "Suspending thread B" );
-			tB.Suspend();
-			Thread.Sleep( 2500 );
-			System.Console.WriteLine( "Running GC..." );
-			GC.Collect();
-			System.Console.WriteLine( "Suspending main thread" );
-			Thread.Sleep( 2500 );
-			tA.Resume();
-			_mreA.Set();
-			Thread.Sleep( 2500 );
-			tB.Resume();
-			_mreB.Set();
-
-			return 0;
-		}
-
-		public void ThreadA()
-		{
-			System.Console.WriteLine( "Thread A Started" );
-			_mreA.WaitOne();
-			System.Console.WriteLine( "Thread A Ended" );
-		}
-
-		public void ThreadB()
-		{
-			System.Console.WriteLine( "Thread B Started" );
-			_mreB.WaitOne();
-			System.Console.WriteLine( "Thread B Ended" );
-		}
-
 		public static int Main( string[] astrArguments )
 		{
 			TestClass tc = new TestClass();
-			return tc.Run();
+			int nType = 1;
+			if ( astrArguments.Length > 0 )
+				nType = Convert.ToInt32( astrArguments[ 0 ] );
+
+			switch ( nType )
+			{
+				case 1:
+					new TestClass1().Run();
+					break;
+				case 2:
+					new TestClass2().Run();
+					break;
+				case 3:
+					new TestClass3().Run();
+					break;
+				default:
+					Console.WriteLine( "Unknown test: " + nType );
+					break;
+			}
+
+			return 0;
 		}
 	}
 }
