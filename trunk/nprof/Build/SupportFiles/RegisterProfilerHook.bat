d4244e51c13c5c0cecd55052b0b1fddefd846a52
mmastrac <mmastrac@0dbee6a0-3221-0410-985d-3ba8a6ab8d54>
2003-12-31 21:21:53 +0000

    Tweaking build scripts, better batch files

--- trunk/nprof/Build/SupportFiles/RegisterProfilerHook.bat
+++ trunk/nprof/Build/SupportFiles/RegisterProfilerHook.bat
@@ -1,3 +1,8 @@
-@echo Registering profiler hook...
-@regsvr32 bin/nprof.hook.dll
-@pause
+@echo off
+echo Registering profiler hook...
+start /wait regsvr32 /s bin/nprof.hook.dll
+if errorlevel 1 ( 
+	rem Re-register, but don't be silent this time
+	regsvr32 bin/nprof.hook.dll 
+)
+pause
--- trunk/nprof/Build/SupportFiles/RegisterVSNetAddin.bat
+++ trunk/nprof/Build/SupportFiles/RegisterVSNetAddin.bat
@@ -1,5 +1,13 @@
-echo Registering nprof DLL...
-@regasm /silent /codebase bin/nprof.vsnetaddin.dll
+@echo off
+echo Registering nprof DLL... 
+Setup\regasm /silent /codebase bin/nprof.vsnetaddin.dll 2> %TEMP%\nprof-regasm-output.txt
+if errorlevel 1 (
+	echo.
+	echo === Error while registering .NET assembly ===
+	type %TEMP%\nprof-regasm-output.txt 
+	echo =============================================
+	echo.
+)
 echo Registering nprof as a VS.NET add-in...
-@regedit /s RegisterVSNetAddin.reg
+regedit /s Setup\RegisterVSNetAddin.reg
 pause
