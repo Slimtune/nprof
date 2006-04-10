9247b7cb28cdc3ecd05818a62247edc46df49ecf
staudenmeyer <staudenmeyer@0dbee6a0-3221-0410-985d-3ba8a6ab8d54>
2006-04-10 19:42:43 +0000

    changed releases to Inno Setup installer

--- branches/0_9_1/nprof/Build/SupportFiles/RegisterProfilerHook.bat
+++ branches/0_9_1/nprof/Build/SupportFiles/RegisterProfilerHook.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Registering profiler hook...
-start /wait regsvr32 /s bin/nprof.hook.dll
+start /wait regsvr32 /s nprof.hook.dll
 if errorlevel 1 ( 
 	rem Re-register, but don't be silent this time
-	regsvr32 bin/nprof.hook.dll 
+	regsvr32 nprof.hook.dll 
 )
 pause
