9469926a97a359898a69f8e2d403dcc5f7e9fdca
staudenmeyer <staudenmeyer@0dbee6a0-3221-0410-985d-3ba8a6ab8d54>
2006-11-10 14:58:30 +0000

    added files

---
+++ trunk/nprof/NProf 2.0/App.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows;
+using System.Data;
+using System.Xml;
+using System.Configuration;
+
+namespace NProf_WPF {
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+
+	public partial class App : System.Windows.Application {
+
+	}
+}
